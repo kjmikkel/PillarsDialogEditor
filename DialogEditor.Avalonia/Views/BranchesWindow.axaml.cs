4d3fd8ea7c65362f7420f44d3d6f49de04d08e75
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2026-06-13 22:44:57 +0200

    feat(a11y): add focus-hint bar to BranchesWindow (Gaps item 13)

--- DialogEditor.Avalonia/Views/BranchesWindow.axaml.cs
+++ DialogEditor.Avalonia/Views/BranchesWindow.axaml.cs
@@ -6,12 +6,17 @@ namespace DialogEditor.Avalonia.Views;
 public partial class BranchesWindow : Window
 {
     // Parameterless ctor so the XAML compiler embeds the type (avoids AVLN3000/AVLN3001).
-    public BranchesWindow() => InitializeComponent();
+    public BranchesWindow()
+    {
+        InitializeComponent();
+        HintBar.AttachTo(this);
+    }
 
     public BranchesWindow(BranchesViewModel vm)
     {
         InitializeComponent();
         DataContext = vm;
         CloseButton.Click += (_, _) => Close();
+        HintBar.AttachTo(this);
     }
 }
