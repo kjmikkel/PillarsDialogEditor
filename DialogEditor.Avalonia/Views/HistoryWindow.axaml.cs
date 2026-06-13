d56262bbde36ecf18962366085f60a234785b931
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2026-06-13 23:29:05 +0200

    feat(a11y): add focus-hint bar to HistoryWindow (Gaps item 16)

--- DialogEditor.Avalonia/Views/HistoryWindow.axaml.cs
+++ DialogEditor.Avalonia/Views/HistoryWindow.axaml.cs
@@ -6,11 +6,16 @@ namespace DialogEditor.Avalonia.Views;
 public partial class HistoryWindow : Window
 {
     // Parameterless ctor so the XAML compiler embeds the type (avoids AVLN3000).
-    public HistoryWindow() => InitializeComponent();
+    public HistoryWindow()
+    {
+        InitializeComponent();
+        HintBar.AttachTo(this);
+    }
 
     public HistoryWindow(HistoryViewModel vm)
     {
         InitializeComponent();
+        HintBar.AttachTo(this);
         DataContext = vm;
         CloseButton.Click += (_, _) => Close();
     }
