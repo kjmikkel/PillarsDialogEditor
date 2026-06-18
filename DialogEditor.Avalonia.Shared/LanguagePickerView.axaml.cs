7ec3fb5493a31e162fb21069a44424b86a1aeb6d
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2026-06-18 09:08:03 +0200

    feat(localisation): add LanguagePickerView mirroring ThemePickerView

---
+++ DialogEditor.Avalonia.Shared/LanguagePickerView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace DialogEditor.Avalonia.Shared;
+
+public partial class LanguagePickerView : UserControl
+{
+    public LanguagePickerView() => InitializeComponent();
+}
